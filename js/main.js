010c446d0c8e90712fcd5ef5c0eba1d619586d65
adelinashahaj <adelinarucaj@icloud.com>
2023-03-16 15:08:44 +0100

    stampato la lista su html

--- js/main.js
+++ js/main.js
@@ -5,7 +5,22 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      
+        
+      tasklist: [
+        {
+            text: 'erert',
+            done: false
+        },
+        {
+            text: 'rrtr',
+            done: false
+        },
+        {
+            text: 'rrthzth',
+            done: false
+        }
+       
+      ]
     }
   }
 }).mount('#app')
